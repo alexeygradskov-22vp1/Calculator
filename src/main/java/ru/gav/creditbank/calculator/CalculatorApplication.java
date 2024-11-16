b25190b41233ccf4193227a502550b185ce5c059
Алексей <gradskovaleksej@gmail.com>
2024-11-16 15:41:17 +0300

    Инициализирован проект

---
+++ src/main/java/ru/gav/creditbank/calculator/CalculatorApplication.java
@@ -0,0 +1,14 @@
+package ru.gav.creditbank.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+}
